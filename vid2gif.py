533d8503053101cb8df021ba407f8cc70afc5e38
Niraporn Kovitaya <bam_28@hotmail.com>
2022-06-03 15:49:57 +0700

    Dockerize

--- vid2gif.py
+++ vid2gif.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python3
 import glob
+
 import cv2
 import sys
 
 from PIL import Image
 
 
-def get_frames():
-    vidcap = cv2.VideoCapture('./assets/test3.mp4')
+def get_frames(fp_in):
+    vidcap = cv2.VideoCapture(fp_in)
 
     fps = vidcap.get(cv2.CAP_PROP_FPS)
     length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -15,28 +17,27 @@ def get_frames():
     success, image = vidcap.read()
     count = 0
     while success:
-        if 1/3*length < count < 1/3*length+fps*15:
-            cv2.imwrite("./assets/images/frame%d.png" % count, image)
+        if 1 / 3 * length < count < 1 / 3 * length + fps * 15:
+            cv2.imwrite("/images/frame%d.png" % count, image)
         success, image = vidcap.read()
         count += 1
     return fps
 
 
-def to_gif(fps, fp_in, fp_out):
+def to_gif(fps, fp_out):
     # filepaths
-    fp_in = fp_in + "frame*.png"
-    fp_out = "./assets/out/image.gif"
+    frames = "/images/frame*.png"
 
     # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
 
-    imgs = (Image.open(f) for f in sorted(glob.glob(fp_in)))
+    imgs = (Image.open(f) for f in sorted(glob.glob(frames)))
     img = next(imgs)  # extract first image from iterator
     img.save(fp=fp_out, format='GIF', append_images=imgs,
-             save_all=True, duration=1/fps, loop=0)
+             save_all=True, duration=1 / fps, loop=0)
 
 
 if __name__ == '__main__':
-    fin = sys.argv[1]
-    fout = sys.argv[2]
-    fps = get_frames()
-    to_gif(fps, fin, fout)
+    fp_in = sys.argv[1]
+    fp_out = sys.argv[2]
+    fps = get_frames(fp_in)
+    to_gif(fps, fp_out)
