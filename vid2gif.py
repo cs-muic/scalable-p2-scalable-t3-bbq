23d3b4d3e6f58cdaaa1d2216e4e04872d293f389
Niraporn Kovitaya <bam_28@hotmail.com>
2022-06-03 19:09:36 +0700

    Fix resizing

--- vid2gif.py
+++ vid2gif.py
@@ -3,22 +3,21 @@ import glob
 
 import cv2
 import sys
-
+import imutils
 from PIL import Image
 
 
 def get_frames(fp_in):
     vidcap = cv2.VideoCapture(fp_in)
-
     fps = vidcap.get(cv2.CAP_PROP_FPS)
     length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-    print(length)
 
     success, image = vidcap.read()
     count = 0
     while success:
         if 1 / 3 * length < count < 1 / 3 * length + fps * 15:
-            cv2.imwrite("/images/frame%d.png" % count, image)
+            frame = imutils.resize(image, width=480)
+            cv2.imwrite("/images/frame%d.png" % count, frame)
         success, image = vidcap.read()
         count += 1
     return fps
