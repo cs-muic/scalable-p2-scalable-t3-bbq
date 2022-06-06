68a3503b28d23f0ccb43c82f885a1dbeedeb2dbe
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-06-06 19:56:50 +0700

    generate random string for bucket name

--- worker/extract.py
+++ worker/extract.py
@@ -1,10 +1,11 @@
 import cv2
 import imutils
 import sys
+import string
+import random
+import os
 from minio import Minio
 from dotenv import load_dotenv
-from PIL import Image
-import os
 
 load_dotenv()
 
@@ -17,35 +18,36 @@ MINIO_API_HOST = "http://localhost:9000"
 MINIO_CLIENT = Minio("localhost:9000", access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
 
 
-def get_frames(fp_in, bucketname):
-    found = MINIO_CLIENT.bucket_exists(bucketname)
+def get_frames(fp_in):
+    s = 10  # number of characters in the string.
+    randstr = ''.join(random.choices(string.ascii_lowercase + string.digits, k=s))
+    found = MINIO_CLIENT.bucket_exists(randstr)
     if not found:
-        MINIO_CLIENT.make_bucket(bucketname)
+        MINIO_CLIENT.make_bucket(randstr)
     else:
-        vidcap = cv2.VideoCapture(fp_in)
-        fps = vidcap.get(cv2.CAP_PROP_FPS)
-        length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-
+        print("Bucket already exists")
+    MINIO_CLIENT.fput_object(randstr, object_name="video.mp4", file_path=fp_in)
+    vidcap = cv2.VideoCapture(fp_in)
+    # fps = vidcap.get(cv2.CAP_PROP_FPS)
+    length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+    success, image = vidcap.read()
+    count = 0
+    while success:
+        if 1 / 3 * length < count < 1 / 3 * length + 15 * 15:
+            frame = imutils.resize(image, width=480)
+            cv2.imwrite("../images/frame%d.png" % count, frame)
+            MINIO_CLIENT.fput_object(randstr, object_name="frame%d.png" % count,
+                                     file_path="../images/frame%d.png" % count)
+            try:
+                os.remove("../images/frame%d.png" % count)
+            except:
+                pass
         success, image = vidcap.read()
-        count = 0
-        while success:
-            if 1 / 3 * length < count < 1 / 3 * length + fps * 15:
-                frame = imutils.resize(image, width=480)
-                cv2.imwrite("../images/frame%d.png" % count, frame)
-                MINIO_CLIENT.fput_object(bucketname, object_name="frame%d.png" % count,
-                                         file_path="../images/frame%d.png" % count)
-                try:
-                    os.remove("../images/frame%d.png" % count)
-                except:
-                    pass
-            success, image = vidcap.read()
-            count += 1
+        count += 1
 
-        print("Successfully uploaded all frames to bucket")
-    return fps
+    return print("Successfully uploaded all frames to bucket")
 
 
 if __name__ == '__main__':
     fp_in = sys.argv[1]
-    bucketname = sys.argv[2]
-    get_frames(fp_in, bucketname)
+    get_frames(fp_in)
