acb83b9c6402356863e31070b1dd1fb01627596f
Niraporn Kovitaya <bam_28@hotmail.com>
2022-06-12 11:09:05 +0700

    Try creating web-controller

--- work-queue/backend.py
+++ work-queue/backend.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from wq_service import *
+from worker import wq_service
 
 app = Flask(__name__)
 
@@ -12,18 +12,24 @@ def index():
 # submit jobs to the queue
 @app.route('/convert')
 def convert():
-    run_task('extract', ['../../test3.mp4'])
-    return "Hello Video Thumbnailer!!"
+    return wq_service.add_to_q("./test3.mp4")
 
 
-# submit jobs to the queue
-# @app.route('/convert')
-# def index():
-#     return "Hello Video Thumbnailer!!"
+@app.route('/convert-bucket')
+def convert_all():
+    return "Converting all videos inside the bucket"
 
 
-# app.run(host='0.0.0.0',port=5000, debug=True)
+@app.route('/list-gif')
+def list_gif():
+    return wq_service.list_gif()
+
+
+@app.route('/track')
+def track():
+    return
+
+
 if __name__ == "__main__":
     from waitress import serve
-
     serve(app, host="0.0.0.0", port=5000)
--- work-queue/worker/wq_service.py
+++ work-queue/worker/wq_service.py
@@ -1,20 +1,62 @@
+import os
+import time
+
+from celery import Celery
 from celery.result import AsyncResult
+from dotenv import load_dotenv
+from minio import Minio
 
 import compose
 import extract
 
+BROKER_URL = os.environ.get("CELERY_BROKER_URL",
+                            "redis://localhost:6378/0"),
+RES_BACKEND = os.environ.get("CELERY_RESULT_BACKEND",
+                             "db+postgresql://dbc:dbc@localhost:5434/celery")
 
-def issue_tasks(worker, tasks):
-    extract_q = []
-    compose_q = []
-    for param in tasks:
-        if worker == 'extract':
-            task = extract.celery_app.send_task('extract.get_frames', queue='q01', kwargs={'fp_in': param})
-            extract_q.append(task.task_id)
-        elif worker == 'compose':
-            task = compose.celery_app.send_task('compose.to_gif', queue='q02', kwargs={'bucket_name': param})
-            compose_q.append(task.task_id)
-    return extract_q, compose_q
+celery_app = Celery('extract', broker=BROKER_URL,
+                    backend=RES_BACKEND)
+
+load_dotenv()
+
+LOCAL_FILE_PATH = os.environ.get('LOCAL_FILE_PATH')
+ACCESS_KEY = os.environ.get('ACCESS_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+MINIO_API_HOST = "http://localhost:9000"
+MINIO_URL = os.environ.get("MINIO_URL")
+
+MINIO_CLIENT = Minio(MINIO_URL, access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
+
+
+def issue_tasks(worker, param):
+    global extract_q, compose_q
+    if worker == 'extract':
+        task = extract.celery_app.send_task('extract.get_frames', queue='q01', kwargs={'fp_in': param})
+        extract_q.append(task.task_id)
+        return task.task_id
+    elif worker == 'compose':
+        task = compose.celery_app.send_task('compose.to_gif', queue='q02', kwargs={'bucket_name': param})
+        compose_q.append(task.task_id)
+        return task.task_id
+
+
+def add_to_eq(video):
+    issue_tasks('extract', video)
+
+
+def convert_bucket(bucket_name):
+    videos = MINIO_CLIENT.list_objects(bucket_name=bucket_name)
+    for vid in videos:
+        add_to_eq(vid)
+
+
+def list_gif():
+    return MINIO_CLIENT.list_objects(bucket_name='gif')
+
+
+def get_progress(tid):
+    return get_results(tid)
 
 
 def get_results(task_id):
@@ -27,9 +69,14 @@ def get_results(task_id):
     return result
 
 
-def run_task(worker, tasks):
-    extract_q, compose_q = issue_tasks(worker, tasks)
+if __name__ == "__main__":
+
+    extract_q = []
+    compose_q = []
     buckets = []
+
+    add_to_eq("./test3.mp4")
+
     while 1:
         done = True
         for tid in extract_q:
@@ -37,28 +84,22 @@ def run_task(worker, tasks):
             if res['task_status'] == 'SUCCESS':
                 buckets.append(res["task_result"])
                 extract_q.remove(tid)
-                _, temp = issue_tasks('compose', buckets)
-                compose_q.extend(temp)
-                print(res["task_result"])
+                compose_q.append(issue_tasks('compose', res["task_result"]))
                 print(f'queue: q01, task: {tid}, result: {res["task_result"]}')
             elif res['task_status'] == 'FAILURE':
                 print(f'queue: q01, task: {tid}, result: {res["task_result"]}')
             else:
                 done = False
                 print(f'queue: q01, task: {tid}, status: {res["task_status"]}')
+                time.sleep(5)
 
         for tid in compose_q:
             res = get_results(tid)
             if res['task_status'] == 'SUCCESS' or \
                     res['task_status'] == 'FAILURE':
-                print(f'queue: q02, task: {tid}, result: {res["task_result"]}')
                 compose_q.remove(tid)
+                print(f'queue: q02, task: {tid}, result: {res["task_result"]}')
             else:
                 done = False
                 print(f'queue: q02, task: {tid}, status: {res["task_status"]}')
-        if done:
-            break
-
-
-if __name__ == '__main__':
-    run_task('extract', ['./test3.mp4'])
+                time.sleep(5)
