438820d50bf9c53651fc8a4b359c46c329acaf02
Niraporn Kovitaya <bam_28@hotmail.com>
2022-06-14 14:08:15 +0700

    Move queue tracker(eq, cq) into database

--- web-controller/backend.py
+++ web-controller/backend.py
@@ -2,7 +2,9 @@ import json
 
 from flask import request, Flask
 from celery.result import AsyncResult
+from flask_sqlalchemy import SQLAlchemy
 from minio import Minio
+from sqlalchemy_utils import create_database, database_exists
 import os
 
 from work_queue.worker.extract_w import extract
@@ -14,7 +16,23 @@ MINIO_URL = os.environ.get("MINIO_URL")
 
 MINIO_CLIENT = Minio(MINIO_URL, access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
 
+url = os.environ.get("RESULT_BACKEND")
+if not database_exists(url):
+    create_database(url)
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = url
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    __tablename__ = 'result'
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.String(length=100))
+    task_type = db.Column(db.String(length=100))
+
+
+db.create_all()
 
 
 @app.route('/')
@@ -43,7 +61,8 @@ def put_video():
     bucket_name = request.form['request']
     f = request.files['file']
     f.save(f.filename)
-    MINIO_CLIENT.fput_object(bucket_name=bucket_name, object_name=f.filename, file_path=f.filename, content_type= f.content_type)
+    MINIO_CLIENT.fput_object(bucket_name=bucket_name, object_name=f.filename, file_path=f.filename,
+                             content_type=f.content_type)
     os.remove(f.filename)
     # MINIO_CLIENT.put_object(bucket_name=bucket_name, object_name=f.name, data=f.stream, content_type=f.content_type, length=f.content_length)
     return 'file uploaded successfully'
@@ -52,23 +71,31 @@ def put_video():
 # submit jobs to the queue
 @app.route('/convert', methods=['POST'])
 def convert():
-    global eq
     video = request.json["video"]
     bucket = request.json["bucket_name"]
     task = extract.celery_app.send_task('extract.get_frames', queue='q01', kwargs={'video': video, 'bucket': bucket})
-    eq.append(task.task_id)
+    new = Task(task_id=task.task_id, task_type="extract")
+    db.session.add(new)
+    try:
+        db.session.commit()
+    except:
+        db.session.rollback()
     return "Converting"
 
 
 @app.route('/convert-bucket', methods=['POST'])
 def convert_all():
-    global eq
     bucket_name = request.json["bucket_name"]
     all_vid = MINIO_CLIENT.list_objects(bucket_name=bucket_name)
     for vid in all_vid:
         MINIO_CLIENT.fget_object(bucket_name=bucket_name, object_name=vid.object_name, file_path=vid.object_name)
         task = extract.celery_app.send_task('extract.get_frames', queue='q01', kwargs={'fp_in': vid.object_name})
-        eq.append(task.task_id)
+        new = Task(task_id=task.task_id, task_type="extract")
+        db.session.add(new)
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
     return "Converting all videos inside the bucket"
 
 
@@ -81,12 +108,22 @@ def list_gif():
 
 @app.route('/track', methods=['POST'])
 def track():
-    global eq, cq
-
-    for task in eq:
-        res = AsyncResult(task)
-        if (res.state == "SUCCESS") and (res.result not in cq):
-            cq.append(res.result)
+    eq = []
+    cq = []
+    for task in Task.query.all():
+        res = AsyncResult(task.task_id)
+        if (task.task_type == "extract") and (res.state == "SUCCESS") and (res.result not in cq):
+            new = Task(task_id=res.result, task_type="compose")
+            db.session.add(new)
+            try:
+                db.session.commit()
+            except:
+                db.session.rollback()
+            # cq.append(res.result)
+        if task.task_type == "extract" and res.result not in cq:
+            eq.append(task.task_id)
+        elif task.task_type == "compose" and task.task_id not in cq:
+            cq.append(task.task_id)
     obj = json.dumps({"extract_q": eq, "compose_q": cq})
 
     try:
@@ -102,8 +139,6 @@ def track():
 
 
 if __name__ == "__main__":
-    eq = []
-    cq = []
     from waitress import serve
 
     serve(app, host="0.0.0.0", port=5000)
